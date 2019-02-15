deee1edf7f48a41ac63e65312536597724031d6a
u48gmv <george-michael.mavridis@bg-phoenics.de>
2019-02-15 21:52:02 +0100

    First working version with settings file

---
+++ public/settings.js
@@ -0,0 +1,23 @@
+appMeldsettings = {};
+appMeldsettings.areaOptions = [
+    {"key":"ALL", "text":"Alle"},        
+    {"key":"ZEN", "text":"GDA ZEN"},
+    {"key":"BGB", "text":"GDA BGB"},
+    {"key":"BGN", "text":"GDA BGN"},
+    {"key":"RCI", "text":"GDA RCI"}
+  ];
+
+appMeldsettings.projectOptions = [
+    {"key":"1", "text":"AT"},
+    {"key":"2", "text":"DS"},
+    {"key":"3", "text":"EA"},
+    {"key":"4", "text":"LW"},
+    {"key":"5", "text":"MB"},
+    {"key":"6", "text":"SE"},
+    {"key":"7", "text":"ST"},
+    {"key":"8", "text":"TA"},
+    {"key":"9", "text":"TT"},
+    {"key":"10", "text":"ZW"}
+];
+
+
--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,15 @@ import * as React from 'react';
 import {PrimaryButton} from 'office-ui-fabric-react/lib/Button';
 import {Dropdown, IDropdownOption} from 'office-ui-fabric-react/lib/Dropdown';
 
+
 import './App.css';
 
-class App extends React.Component<{},{selectedArea:{key:string|number},selectedProject:{key:string|number}}> {
+declare var appMeldsettings: any;
 
+
+class App extends React.Component<{},{selectedArea:{key:string|number},selectedProject:{key:string|number}}> {
+  
+ 
   constructor(props: {}){
     super(props);
     this.state = {
@@ -15,16 +20,15 @@ class App extends React.Component<{},{selectedArea:{key:string|number},selectedP
   }
 
   public areaChange = (event: React.FormEvent<HTMLDivElement>, item: IDropdownOption): void => {
-    console.log('areaChange => '+item.key);
     this.setState({selectedArea:item});
   }
 
   public projectChange = (event: React.FormEvent<HTMLDivElement>, item: IDropdownOption): void => {
-    console.log('projectChange => '+item.key);
     this.setState({selectedProject:item});
   }
 
   public render() {
+    const buttonActive = this.checkJumpButton();
 
     return (
       <div className="ms-Grid" dir="ltr">
@@ -35,39 +39,23 @@ class App extends React.Component<{},{selectedArea:{key:string|number},selectedP
           ariaLabel="Bereich auswählen"
           onChange={this.areaChange}        
           placeholder="Wähle einen Bereich"
-          options={[
-            {"key":"ALL", "text":"Alle"},
-            {"key":"ZEN", "text":"GDA ZEN"},
-            {"key":"BGB", "text":"GDA BGB"},
-            {"key":"BGN", "text":"GDA BGN"},
-            {"key":"RCI", "text":"GDA RCI"}
-          ]}
+          options={appMeldsettings.areaOptions}
           />        
         </div>
         <div className="ms-Grid-col ms-sm4">
           <Dropdown
           label="Projekt auswählen"
           ariaLabel="Projekt auswählen"
-          placeholder="Wähle einen Projekt"
+          placeholder="Wähle ein Projekt"
           onChange={this.projectChange}
-          disabled={(this.state.selectedArea.key === "ALL" || this.state.selectedArea.key === "") ? true : false}
-          options={[
-            {"key":"1", "text":"AT"},
-            {"key":"2", "text":"DS"},
-            {"key":"3", "text":"EA"},
-            {"key":"4", "text":"LW"},
-            {"key":"5", "text":"MB"},
-            {"key":"6", "text":"SE"},
-            {"key":"7", "text":"ST"},
-            {"key":"8", "text":"TA"},
-            {"key":"9", "text":"TT"},
-            {"key":"10", "text":"ZW"}          
-          ]}
+          disabled={(this.checkArea()) ? false : true}
+          options={appMeldsettings.projectOptions}
           />        
         </div>
         <div className="ms-Grid-col ms-sm4 app-meld-button-wrapper">
           <PrimaryButton 
             text="Los"
+            disabled={buttonActive}
             onClick={this.jumpToDestination}
           />        
         </div>                
@@ -77,16 +65,55 @@ class App extends React.Component<{},{selectedArea:{key:string|number},selectedP
     );
   }
 
+ 
+
   private jumpToDestination = (): void => {
     let hrefToJump = "https://www.example.com";
+    const selectedArea = this.state.selectedArea.key;
+    const selectedProject = this.state.selectedProject.key;
+    const spRootFolderInfo = "RootFolder=";    
 
-    if(this.state.selectedArea.key !== "ALL" && this.state.selectedArea.key !== ""){
-      hrefToJump = "https://www.exmple.com/something/new"
+    if(this.checkArea()){
+      hrefToJump = selectedProject+spRootFolderInfo+selectedArea;
     }
 
     console.log(hrefToJump);
     // window.location.href = hrefToJump;
-  }    
+  }
+
+  private checkArea = (): boolean => {
+    let toReturn = true;
+    const selectedArea = this.state.selectedArea.key;
+
+    if(selectedArea === "ALL" || selectedArea === ""){
+      toReturn = false;
+    }
+
+    return toReturn;
+  }
+
+  private checkProject = (): boolean => {
+    let toReturn = true;
+    const selectedProject = this.state.selectedProject.key;
+
+    if(selectedProject === ""){
+      toReturn = false;
+    }
+
+    return toReturn;
+  }  
+
+  private checkJumpButton = (): boolean => {
+    let toReturn = true;
+
+    if(this.state.selectedArea.key === 'ALL' || (this.state.selectedArea.key !== 'ALL' && this.checkProject())){
+      toReturn = false;
+    }
+
+    return toReturn;
+  } 
+  
+
 }
 
 export default App;
